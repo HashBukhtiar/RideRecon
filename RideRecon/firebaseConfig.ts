8dddfeba4d6c9b7d72945097a52db101d076ecb2
Jaden Moore <jadenmoore@Jadens-MacBook-Pro.local>
2025-03-30 19:57:01 -0400

    key

--- RideRecon/firebaseConfig.ts
+++ RideRecon/firebaseConfig.ts
@@ -8,13 +8,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAoqaaBiKyaDY22zosBPOdJWgUN7c_CkXg",
-  authDomain: "riderecon-2c366.firebaseapp.com",
-  projectId: "riderecon-2c366",
-  storageBucket: "gs://riderecon-2c366.firebasestorage.app",
-  messagingSenderId: "914098900484",
-  appId: "1:914098900484:web:d16f52dba9053bbb0e2f51",
-  measurementId: "G-RQ1CE4F8XT"
+
 };
 
 // Initialize Firebase
